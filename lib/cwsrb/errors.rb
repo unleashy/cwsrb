69d6f1f828604be9318e496b1cd2dce77aacb83c
unleashy <lakeavenger@gmail.com>
2016-09-30 15:04:53 -0300

    Remove this useless bs

--- lib/cwsrb/errors.rb
+++ lib/cwsrb/errors.rb
@@ -1,12 +1,8 @@
 module Cwsrb
-  # @!group Errors
-
   # @abstract This class subclasses `RuntimeError` and serves as a base
   #   for all other CWSrb's error classes.
   Error = Class.new(RuntimeError)
 
   # Raised when a general API error occurs.
   APIError = Class.new(Error)
-
-  # @!endgroup
 end
